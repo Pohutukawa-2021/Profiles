45552bcf5d1c4e6c30060276b7f3cf81f4248eae
LouisDVL <louis.lising@outlook.com>
2021-08-13 10:17:10 +1200

    checkpoint

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require("express");
+const hbs = require("express-handlebars");
+
+const server = express();
+
+//server config
+server.use(express.static("public"));
+server.use(express.urlencoded({ extended: false }));
+
+//handlebars config
+server.engine("handlebars", hbs({ extname: "hbs" }));
+server.set("view engine", "handlebars");
+
+server.get("/", (req, res) => {
+  res.render("copy");
+});
+
+module.exports = {
+  server,
+};
