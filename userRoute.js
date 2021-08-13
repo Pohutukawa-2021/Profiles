c036dac4ce98f64a0ef1c582ec3203946c5025c9
LouisDVL <louis.lising@outlook.com>
2021-08-13 12:57:29 +1200

    deleted package lock file

---
+++ userRoute.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { getData } = require("./get_utils");
+
+const router = express.Router();
+
+module.exports = router;
+
+router.get("/new", (req, res) => {
+  res.render("create");
+});
+
+router.post("/new", (req, res) => {
+  let newUser = { ...req.body };
+  getData((err, data) => {
+    if (err) {
+      console.log("There was a problem with getting the data");
+    }
+    let newData = { ...data, users: [...data.users, newUser] };
+    console.log(newData);
+  });
+});
