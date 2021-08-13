c73b9c4238b81b4417e523e774aa6d279d1a9eb1
LouisDVL <louis.lising@outlook.com>
2021-08-13 12:24:00 +1200

    checkpoint

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 
-const { getData } = require('./get_utils')
+const { getData } = require("./get_utils");
 
 const server = express();
+
+//const routes
+const userRoutes = require("./userRoute");
 
 //server config
 server.use(express.static("public"));
@@ -16,16 +19,18 @@ server.set("view engine", "hbs");
 server.get("/", (req, res) => {
   getData((err, profiles) => {
     if (err) {
-      res.status(500).send(err.message)
-      return
+      res.status(500).send(err.message);
+      return;
     }
     const viewData = {
-      profiles: profiles
-    }
+      profiles: profiles,
+    };
 
     res.render("home", profiles);
-  })
+  });
 });
+
+server.use("/user", userRoutes);
 
 module.exports = {
   server,
