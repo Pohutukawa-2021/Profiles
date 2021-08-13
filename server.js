c6633d267a6ca5480cc59e4a65281239d930422b
LouisDVL <louis.lising@outlook.com>
2021-08-13 14:04:40 +1200

    changed create form

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 const updateJson = require("./post-utils");
-<<<<<<< HEAD
 const userRouter = require("./userRoute");
-=======
-const routes = require('./userRoute')
->>>>>>> 8fdd67c675ae7a8728f97be9cd79c7a139c3136d
 
 const { getData } = require("./get_utils");
 
 const server = express();
 
-//const routes
-const userRoutes = require("./userRoute");
-
 //server config
 server.use(express.static("public"));
 server.use(express.urlencoded({ extended: false }));
-server.use('/user', routes)
 
 //handlebars config
 server.engine("hbs", hbs({ extname: "hbs" }));
@@ -37,11 +29,7 @@ server.get("/", (req, res) => {
   });
 });
 
-<<<<<<< HEAD
 server.use("/user", userRouter);
-=======
-server.use("/user", userRoutes);
->>>>>>> dede2456d038582d50e9e663fe45a91527557ab1
 
 module.exports = {
   server,
